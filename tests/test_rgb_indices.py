718e429de1b68fbf6e6d5087fcab422fc6923eec
Jacob van der Leeuw <jvanderleeuw@email.arizona.edu>
2020-08-07 10:23:38 -0700

    testing

--- tests/test_rgb_indices.py
+++ tests/test_rgb_indices.py
@@ -10,6 +10,8 @@ import os
 import re
 from subprocess import getstatusoutput
 
+import testing
+
 SOURCE_FILE = 'testing.py'
 IMAGES = 'images'
 SOURCE_PATH = os.path.abspath(os.path.join('.', SOURCE_FILE))
@@ -31,5 +33,8 @@ def test_usage():
 
 
 def test_arguments():
+    """
+    Tests testing.py's check_arguments() function
+    """
     import testing
     return testing.check_arguments()
