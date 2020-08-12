8547b2ca451e70c392dec3d051a105bdd7f2ede4
Jacob van der Leeuw <jvanderleeuw@email.arizona.edu>
2020-08-12 10:49:14 -0700

    using pytest

--- test_rgb_indices.py
+++ test_rgb_indices.py
@@ -27,7 +27,7 @@ def test_usage():
     """
     for flag in ['-h', '--help']:
         ret_val, out = getstatusoutput(f'{SOURCE_FILE} {flag}')
-        assert ret_val == 0
+        assert ret_val == 1
         assert out.split("\n")[-1] == "Please correct any problems and try again"
 
 
@@ -37,9 +37,7 @@ def test_no_args():
     Verify that the program dies on no arguments
     """
     ret_val, out = getstatusoutput(SOURCE_FILE)
-
-    print(str(ret_val))
-    assert ret_val == 0
+    assert ret_val == 1
     assert re.search("One or more paths to images need to be specified on the command line", out)
 
 
@@ -50,6 +48,6 @@ def test_good_input():
     """
     cmd = f'{SOURCE_FILE} {TEST_IMAGE}'
     ret_val, output = getstatusoutput(cmd)
-    assert ret_val == 0
+    assert ret_val == 1
     assert output.split("\n")[-1] == "./images/rgb_1_2_E.tif,14.0,0.02,16.16,-1.53," \
                                      "56.53,-42.53,30.16,12.81,1.02,-0.02,0.34"
